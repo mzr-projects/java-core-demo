8fd11b59270717ba51de0cc3e4ca7aa5a065809f
Maziar <taghipour.maziar@gmail.com>
2022-03-23 01:03:09 -0700

    Lambdas

--- src/main/java/com/mt/lambdas/LambdaApp.java
+++ src/main/java/com/mt/lambdas/LambdaApp.java
@@ -2,12 +2,26 @@ package com.mt.lambdas;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class LambdaApp {
 	public static void main(String[] args) {
 		StringSorter stringSorter = new StringSorter();
-		List<String> strings = Arrays.asList("Holy Noly", "James", "What the fuck is this ?", "TT");
+		List<String> strings = Arrays.asList("Holy Moly", "James", "What the fuck is this ?", "TT");
 		stringSorter.setStrings(strings);
 		System.out.println(stringSorter.lengthSort());
+
+		System.out.println("============== Stream Definition");
+		Stream.of(3, 1, 4, 2, 6, 7).forEach(n -> System.out.println(n));
+
+		System.out.println("============= Consumer Definition");
+		Consumer<Integer> printer = n -> System.out.println(n);
+		Stream.of(3, 1, 4, 2, 6, 7).forEach(printer);
+
+		System.out.println("============ Predicate % 3");
+		Predicate<Integer> integerPredicate = n -> n % 3 == 0;
+		Stream.of(3, 2, 1, 4, 6, 7, 8, 9, 21).filter(integerPredicate).forEach(System.out::println);
 	}
 }
