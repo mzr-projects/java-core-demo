8d138666f08f2414568cad67084a4fe145791be3
Maziar Taghipour <taghipour.maziar@yahoo.com>
2023-01-19 02:14:03 -0800

    Big commit to sync with the latest changes

--- src/main/java/com/mt/designPatterns/proxy/RealObject.java
+++ src/main/java/com/mt/designPatterns/proxy/RealObject.java
@@ -1,6 +1,7 @@
 package com.mt.designPatterns.proxy;
 
-public class RealObject implements Work{
+public class RealObject implements Work {
+
 	@Override
 	public void doSomething() {
 		System.out.println("Doing something in RealObject");
