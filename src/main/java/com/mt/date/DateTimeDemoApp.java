a30450ca3b82eede9e5e5a5309ba3762a4d993a0
Maziar Taghipour <taghipour.maziar@yahoo.com>
2023-01-21 10:22:59 -0800

    changes in liskov principle

--- src/main/java/com/mt/date/DateTimeDemoApp.java
+++ src/main/java/com/mt/date/DateTimeDemoApp.java
@@ -1,13 +1,17 @@
 package com.mt.date;
 
+import java.time.LocalDate;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
 public class DateTimeDemoApp {
     public static void main(String[] args) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
         ZonedDateTime zonedDateTime = ZonedDateTime.parse("2023-05-01 10:15:30 Asia/Tehran", formatter);
         System.out.println(zonedDateTime);
+
+        String dateInString = "20230506";
+        LocalDate date = LocalDate.parse(dateInString, DateTimeFormatter.BASIC_ISO_DATE);
+        System.out.println(date);
     }
 }
